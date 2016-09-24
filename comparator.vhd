738625c1ddfc9cdb63672802641cc9ec4716f56c
vasu gankidi <vasu.gankidi@gmail.com>
2016-09-24 13:40:35 -0700

    comment added and size changed

--- comparator.vhd
+++ comparator.vhd
@@ -1,9 +1,10 @@
+--git is tested here
 library ieee;
 use ieee.std_logic_1164.all;
 entity Comparator is 
 generic (size : integer := 6);
 port (dina: in std_logic_vector(size-1 downto 0);
-	  dinb: in std_logic_vector(size -1 downto 0);
+	  dinb: in std_logic_vector(size -2 downto 0);
 	  aEQb : out std_logic 
 	   );
 end Comparator;
@@ -12,7 +13,7 @@ architecture Comparator of Comparator is
 begin 
 process (dina,dinb)
 begin
-if (dina = dinb) then 
+if (dina != dinb) then 
 aEQb <= '1';
 else 
 aEQb <= '0';
